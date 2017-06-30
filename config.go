2c76e847339fde64d870ac73ad6c6c282825e8a7
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-07-01 01:09:17 +0200

    move

--- config.go
+++
@@ -1,7 +0,0 @@
-package naos
-
-const espIDFVersion = "cc93e14770e7b3681ebc80b30336e498cc96e961"
-
-const espMQTTVersion = "cc87172126aa7aacc3b982f7be7489950429b733"
-
-const espLibVersion = "8c9e924556b21f329da12151b18570b62517d3f5"
--- project.go
+++ project.go
@@ -14,6 +14,12 @@ import (
 	"github.com/mholt/archiver"
 )
 
+const espIDFVersion = "cc93e14770e7b3681ebc80b30336e498cc96e961"
+
+const espMQTTVersion = "cc87172126aa7aacc3b982f7be7489950429b733"
+
+const espLibVersion = "8c9e924556b21f329da12151b18570b62517d3f5"
+
 // TODO: Make inventory file configurable?
 
 const inventoryFileName = "naos.json"
