745277eb5d2cf22a39d35ee81549c5c4e39823b7
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-06-30 17:42:08 +0200

    updated to latest lib version

--- config.go
+++ config.go
@@ -7,4 +7,4 @@ const ESPIDFVersion = "cc93e14770e7b3681ebc80b30336e498cc96e961"
 const ESPMQTTVersion = "cc87172126aa7aacc3b982f7be7489950429b733"
 
 // ESPLibVersion defines the used 'naos-esp' version.
-const ESPLibVersion = "4bcc858f77671f0f3846fe9c704cb95fea34ea1f"
+const ESPLibVersion = "8c9e924556b21f329da12151b18570b62517d3f5"
