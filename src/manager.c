c66255d2c09cd00c3303575bf1e06fad21d62692
Joël Gähwiler <joel.gaehwiler@gmail.com>
2018-04-10 23:15:58 +0200

    use already null terminated payload

--- src/manager.c
+++ src/manager.c
@@ -307,11 +307,7 @@ void naos_manager_handle(const char *topic, uint8_t *payload, size_t len, naos_s
     naos_set(param, (const char *)payload);
 
     // update task
-    if (len > 0) {
-      naos_task_update(param, (const char *)payload);
-    } else {
-      naos_task_update(param, "");
-    }
+    naos_task_update(param, (const char *)payload);
 
     // construct topic
     char *t = naos_str_concat("naos/value/", param);
