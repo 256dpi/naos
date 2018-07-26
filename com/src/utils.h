7cbdba6cb7532dfe39ad13f18758a9f0f4306efc
Joël Gähwiler <joel.gaehwiler@gmail.com>
2018-07-26 12:03:43 +0200

    addec config option to debug internal locks

--- com/src/utils.h
+++ com/src/utils.h
@@ -1,6 +1,7 @@
 #ifndef _NAOS_GENERAL_H
 #define _NAOS_GENERAL_H
 
+#include <esp_log.h>
 #include <stdint.h>
 
 /**
@@ -13,16 +14,30 @@
  *
  * @param mutex The mutex to be locked.
  */
+#ifdef CONFIG_NAOS_DEBUG_LOCKS
+#define NAOS_LOCK(mutex)                                    \
+  ESP_LOGI(NAOS_LOG_TAG, "NAOS_LOCK: %s", __func__);        \
+  do {                                                      \
+  } while (xSemaphoreTake(mutex, portMAX_DELAY) != pdPASS); \
+  ESP_LOGI(NAOS_LOG_TAG, "NAOS_LOCKED: %s", __func__)
+#else
 #define NAOS_LOCK(mutex) \
   do {                   \
   } while (xSemaphoreTake(mutex, portMAX_DELAY) != pdPASS)
+#endif
 
 /**
  * Release the specified mutex.
  *
  * @param mutex The mutex to be released.
  */
+#ifdef CONFIG_NAOS_DEBUG_LOCKS
+#define NAOS_UNLOCK(mutex)                             \
+  ESP_LOGI(NAOS_LOG_TAG, "NAOS_UNLOCK: %s", __func__); \
+  xSemaphoreGive(mutex)
+#else
 #define NAOS_UNLOCK(mutex) xSemaphoreGive(mutex)
+#endif
 
 /**
  * Convert number to string.
