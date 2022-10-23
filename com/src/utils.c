16f3ef3dc4d98a65c2d298c2ef2edf053b61d3b3
Joël Gähwiler <joel.gaehwiler@gmail.com>
2022-10-23 22:05:11 +0200

    fmt

--- com/src/utils.c
+++ com/src/utils.c
@@ -15,34 +15,33 @@ void naos_delay(uint32_t millis) {
 }
 
 const char *naos_i2str(int32_t num) {
+  // convert
   static char str[16] = {0};
   snprintf(str, 16, "%d", num);
+
   return str;
 }
 
 const char *naos_d2str(double num) {
+  // convert
   static char str[16] = {0};
   snprintf(str, 16, "%.4f", num);
+
   return str;
 }
 
 char *naos_format(char *fmt, ...) {
+  // measure
   va_list va;
-
-  // get length
   va_start(va, fmt);
   int ret = vsnprintf(NULL, 0, fmt, va);
   va_end(va);
 
-  // allocate
-  char *buf = malloc(ret + 1);
-
   // format
+  char *buf = malloc(ret + 1);
   va_start(va, fmt);
   ret = vsnprintf(buf, ret + 1, fmt, va);
   va_end(va);
-
-  // check ret
   if (ret < 0) {
     ESP_ERROR_CHECK(ESP_FAIL);
   }
@@ -51,13 +50,9 @@ char *naos_format(char *fmt, ...) {
 }
 
 char *naos_concat(const char *str1, const char *str2) {
-  // allocate new buffer
+  // copy strings
   char *str = malloc(strlen(str1) + strlen(str2) + 1);
-
-  // copy first string
   strcpy(str, str1);
-
-  // copy second string
   strcat(str, str2);
 
   return str;
