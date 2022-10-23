5c66357999e514312b07106e0b96b7db0fc117d4
Joël Gähwiler <joel.gaehwiler@gmail.com>
2022-10-23 16:42:22 +0200

    simplified lock debugging

--- com/src/utils.h
+++ com/src/utils.h
@@ -6,25 +6,15 @@
 
 #define NAOS_LOG_TAG "naos"
 
-#ifdef CONFIG_NAOS_DEBUG_LOCKS
-#define NAOS_LOCK(mutex)                                    \
-  ESP_LOGI(NAOS_LOG_TAG, "NAOS_LOCK: %s", __func__);        \
+#define NAOS_LOCK(mutex) \
+  ESP_LOGV(NAOS_LOG_TAG, "NAOS_LOCK: %s", __func__);        \
   do {                                                      \
   } while (xSemaphoreTake(mutex, portMAX_DELAY) != pdPASS); \
-  ESP_LOGI(NAOS_LOG_TAG, "NAOS_LOCKED: %s", __func__)
-#else
-#define NAOS_LOCK(mutex) \
-  do {                   \
-  } while (xSemaphoreTake(mutex, portMAX_DELAY) != pdPASS)
-#endif
+  ESP_LOGV(NAOS_LOG_TAG, "NAOS_LOCKED: %s", __func__)
 
-#ifdef CONFIG_NAOS_DEBUG_LOCKS
 #define NAOS_UNLOCK(mutex)                             \
-  ESP_LOGI(NAOS_LOG_TAG, "NAOS_UNLOCK: %s", __func__); \
+  ESP_LOGV(NAOS_LOG_TAG, "NAOS_UNLOCK: %s", __func__); \
   xSemaphoreGive(mutex)
-#else
-#define NAOS_UNLOCK(mutex) xSemaphoreGive(mutex)
-#endif
 
 const char *naos_i2str(int32_t num);
 const char *naos_d2str(double num);
