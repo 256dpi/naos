cb36adb3b36d89b3c4c9e0d30f078a9d4867b97e
Joël Gähwiler <joel.gaehwiler@gmail.com>
2022-10-27 16:24:57 +0200

    websocket based http configuration

--- com/src/http.c
+++ com/src/http.c
@@ -1,6 +1,7 @@
 #include <esp_http_server.h>
 
 #include "params.h"
+#include "utils.h"
 
 static httpd_handle_t naos_http_handle = {0};
 
@@ -22,101 +23,133 @@ static esp_err_t naos_http_index(httpd_req_t *req) {
   return ESP_OK;
 }
 
-static esp_err_t naos_http_param(httpd_req_t *req) {
-  // set response header
-  esp_err_t err = httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
-  if (err != ESP_OK) {
-    return err;
-  }
-  err = httpd_resp_set_hdr(req, "Access-Control-Allow-Methods", "*");
-  if (err != ESP_OK) {
-    return err;
-  }
-
-  // handle preflight
-  if (req->method == HTTP_OPTIONS) {
-    return httpd_resp_send(req, NULL, 0);
-  }
-
-  // read name
-  char name[32] = {0};
-  if (httpd_req_get_url_query_len(req) > 0) {
-    char query[64] = {0};
-    if (httpd_req_get_url_query_len(req) > sizeof(query)) {
-      return httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "query too long");
-    }
-    err = httpd_req_get_url_query_str(req, query, sizeof(query));
-    if (err != ESP_OK) {
-      return err;
-    }
-    err = httpd_query_key_value(query, "name", name, sizeof(name));
-    if (err != ESP_OK) {
-      return err;
-    }
-  }
-
-  // handle empty
-  if (strnlen(name, sizeof(name)) <= 0) {
-    // write response
-    char *list = naos_params_list(0);
-    err = httpd_resp_send(req, list, HTTPD_RESP_USE_STRLEN);
-    free(list);
-    if (err != ESP_OK) {
-      return err;
-    }
-
+static esp_err_t naos_http_socket(httpd_req_t *conn) {
+  // handle GET request
+  if (conn->method == HTTP_GET) {
     return ESP_OK;
   }
 
-  // find param
-  naos_param_t *param = naos_lookup(name);
-  if (param == NULL) {
-    return httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "unknown param");
-  }
+  // prepare request frame
+  httpd_ws_frame_t req = {.type = HTTPD_WS_TYPE_TEXT};
 
-  // handle read
-  if (req->method == HTTP_GET) {
-    // read param
-    char *value = strdup(naos_get(name));
-
-    // write value
-    err = httpd_resp_sendstr(req, value);
-
-    // free value
-    free(value);
-
-    return err;
-  }
-
-  /* handle write */
-
-  // read value
-  char value[128] = {0};
-  if (req->content_len > sizeof(value)) {
-    return httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "body too long");
-  }
-  size_t len = httpd_req_recv(req, value, sizeof(value));
-  if (len != req->content_len) {
-    return httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "body too short");
-  }
-  value[len] = 0;
-
-  // write param
-  naos_set(name, value);
-
-  // write response
-  err = httpd_resp_send(req, "OK", HTTPD_RESP_USE_STRLEN);
+  // read request frame length
+  esp_err_t err = httpd_ws_recv_frame(conn, &req, 0);
   if (err != ESP_OK) {
     return err;
   }
 
-  return ESP_OK;
+  // read request frame
+  if (req.len) {
+    // allocate payload
+    req.payload = malloc(req.len + 1);
+    if (req.payload == NULL) {
+      return ESP_ERR_NO_MEM;
+    }
+
+    // read frame
+    err = httpd_ws_recv_frame(conn, &req, req.len);
+    if (err != ESP_OK) {
+      free(req.payload);
+      return err;
+    }
+
+    // terminate payload
+    req.payload[req.len] = 0;
+  }
+
+  // log frame
+  naos_log("frame: %s", req.payload);
+
+  // prepare response
+  httpd_ws_frame_t res = {.type = HTTPD_WS_TYPE_TEXT};
+
+  // handle list
+  if (strncmp((char *)req.payload, "list", 4) == 0) {
+    // list params
+    char *list = naos_params_list(0);
+
+    // set payload
+    res.payload = (uint8_t *)naos_concat("list#", list);
+    res.len = strlen((char *)res.payload);
+
+    // free intermediaries
+    free(list);
+  }
+
+  // handle read
+  if (strncmp((char *)req.payload, "read", 4) == 0) {
+    // get name
+    const char *name = (char *)req.payload + 5;
+
+    // lookup param
+    naos_param_t *param = naos_lookup(name);
+    if (param == NULL) {
+      free(req.payload);
+      return ESP_FAIL;
+    }
+
+    // get value
+    char *value = strdup(naos_get(param->name));
+
+    // set payload
+    res.payload = (uint8_t *)naos_format("read:%s#%s", name, value);
+    res.len = strlen((char *)res.payload);
+
+    // free intermediates
+    free(value);
+  }
+
+  // handle write
+  if (strncmp((char *)req.payload, "write", 5) == 0 && strchr((char *)req.payload, '#') != NULL) {
+    // get name
+    const char *name = (char *)req.payload + 6;
+
+    // replace hash with zero
+    char *hash = strchr(name, '#');
+    *hash = 0;
+
+    // get value
+    char *value = hash + 1;
+
+    // lookup param
+    naos_param_t *param = naos_lookup(name);
+    if (param == NULL) {
+      free(req.payload);
+      return ESP_FAIL;
+    }
+
+    // set value
+    naos_set(param->name, value);
+
+    // get value
+    value = strdup(naos_get(param->name));
+
+    // set payload
+    res.payload = (uint8_t *)naos_format("write:%s#%s", name, value);
+    res.len = strlen((char *)res.payload);
+
+    // free intermediates
+    free(value);
+  }
+
+  // free request payload
+  free(req.payload);
+
+  // send response frame
+  err = httpd_ws_send_frame(conn, &res);
+
+  // free response payload
+  free(res.payload);
+
+  return err;
 }
 
 static httpd_uri_t naos_http_route_index = {.uri = "/", .method = HTTP_GET, .handler = naos_http_index};
-static httpd_uri_t naos_http_route_param_opt = {.uri = "/naos", .method = HTTP_OPTIONS, .handler = naos_http_param};
-static httpd_uri_t naos_http_route_param_get = {.uri = "/naos", .method = HTTP_GET, .handler = naos_http_param};
-static httpd_uri_t naos_http_route_param_put = {.uri = "/naos", .method = HTTP_PUT, .handler = naos_http_param};
+static httpd_uri_t naos_http_route_socket = {.uri = "/naos",
+                                             .method = HTTP_GET,
+                                             .handler = naos_http_socket,
+                                             .is_websocket = true,
+                                             .supported_subprotocol = "naos"};
 
 void naos_http_init() {
   // prepare config
@@ -127,7 +160,5 @@ void naos_http_init() {
 
   // register handlers
   ESP_ERROR_CHECK(httpd_register_uri_handler(naos_http_handle, &naos_http_route_index));
-  ESP_ERROR_CHECK(httpd_register_uri_handler(naos_http_handle, &naos_http_route_param_opt));
-  ESP_ERROR_CHECK(httpd_register_uri_handler(naos_http_handle, &naos_http_route_param_get));
-  ESP_ERROR_CHECK(httpd_register_uri_handler(naos_http_handle, &naos_http_route_param_put));
+  ESP_ERROR_CHECK(httpd_register_uri_handler(naos_http_handle, &naos_http_route_socket));
 }
