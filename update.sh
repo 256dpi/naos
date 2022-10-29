82e2045064a50d26a390d0450fae322b3da60d7b
Joël Gähwiler <joel.gaehwiler@gmail.com>
2022-10-29 16:39:32 +0200

    added esp-osc

--- update.sh
+++ update.sh
@@ -2,15 +2,18 @@
 
 ESP_IDF_VERSION=$(cat ./tree/esp-idf.version)
 ESP_MQTT_VERSION=$(cat ./tree/esp-mqtt.version)
+ESP_OSC_VERSION=$(cat ./tree/esp-osc.version)
 
 printf "\nCurrent Versions:\n"
 printf "==> esp-idf: ${ESP_IDF_VERSION}\n"
 printf "==> esp-mqtt: ${ESP_MQTT_VERSION}\n"
+printf "==> esp-osc: ${ESP_OSC_VERSION}\n"
 
 printf "\nUpdating naos tree...\n"
 cd tree
 cd esp-idf; git fetch; git checkout ${ESP_IDF_VERSION}; git submodule update --recursive; cd ..
 cd components/esp-mqtt; git fetch; git checkout ${ESP_MQTT_VERSION}; git submodule update --recursive; cd ../..
+cd components/esp-osc; git fetch; git checkout ${ESP_OSC_VERSION}; git submodule update --recursive; cd ../..
 cd ..
 
 printf "\nCopying sdkconfig...\n"
