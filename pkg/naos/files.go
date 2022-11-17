7441fe103ec18e914b323b21833d8417a76dc3be
Joël Gähwiler <joel.gaehwiler@gmail.com>
2022-11-17 08:45:41 +0100

    fix default source

--- pkg/naos/files.go
+++ pkg/naos/files.go
@@ -3,7 +3,7 @@ package naos
 const mainSourceFile = `#include <naos.h>
 
 static naos_config_t config = {.device_type = "my-device",
-                               .firmware_version = "0.1.0"};
+                               .device_version = "0.1.0"};
 
 void app_main() {
   // initialize naos
