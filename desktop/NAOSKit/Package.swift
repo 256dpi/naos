940925d5747de23279cc23a0cb9842244de7e667
Joël Gähwiler <joel.gaehwiler@gmail.com>
2023-08-18 11:50:23 +0200

    desktop: updated Semaphore dependency

--- desktop/NAOSKit/Package.swift
+++ desktop/NAOSKit/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/256dpi/AsyncBluetooth", revision: "main"),
-		.package(url: "https://github.com/groue/Semaphore", from: "0.0.6"),
+		.package(url: "https://github.com/groue/Semaphore", from: "0.0.8"),
 	],
 	targets: [
 		.target(
