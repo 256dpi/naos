32409fd9557b72e36c2aa18ddc92d30e14110944
Joël Gähwiler <joel.gaehwiler@gmail.com>
2023-10-22 16:25:14 +0200

    updated test app

--- test/src/main.c
+++ test/src/main.c
@@ -1,4 +1,8 @@
 #include <naos.h>
+#include <naos/ble.h>
+#include <naos/wifi.h>
+#include <naos/mqtt.h>
+#include <naos/manager.h>
 
 extern const uint8_t foo_start[] asm("_binary_foo_txt_start");
 extern const uint8_t foo_end[] asm("_binary_foo_txt_end");
@@ -18,6 +22,10 @@ static naos_config_t config = {
 void app_main() {
   // initialize naos
   naos_init(&config);
+  naos_ble_init((naos_ble_config_t){});
+  naos_wifi_init();
+  naos_mqtt_init(1);
+  naos_manager_init();
 
   // print foo
   naos_log("%d", foo_end - foo_start);
