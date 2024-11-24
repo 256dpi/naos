47dfeb7c7ce3709e648c165b3b71aa0f5512532f
Joël Gähwiler <joel.gaehwiler@gmail.com>
2024-11-24 18:39:06 +0100

    swift: cleaned up imports

--- swift/NAOSKit/Sources/NAOSManagedDevice.swift
+++ swift/NAOSKit/Sources/NAOSManagedDevice.swift
@@ -3,7 +3,6 @@
 //  Copyright © 2017 Joël Gähwiler. All rights reserved.
 //
 
-import Combine
 import Foundation
 import Semaphore
 
--- swift/NAOSKit/Sources/NAOSSession.swift
+++ swift/NAOSKit/Sources/NAOSSession.swift
@@ -3,7 +3,6 @@
 //  Copyright © 2023 Joël Gähwiler. All rights reserved.
 //
 
-import Combine
 import Foundation
 import Semaphore
 
