a99badc33177be7db9532af802a133119595b182
Joël Gähwiler <joel.gaehwiler@gmail.com>
2025-01-25 19:52:57 +0100

    com: use runtime stats to track CPU usage

--- com/src/cpu.c
+++ com/src/cpu.c
@@ -2,59 +2,45 @@
 #include <naos/sys.h>
 #include <naos/metrics.h>
 
-#include <esp_freertos_hooks.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
-// TODO: Use ulTaskGetIdleRunTimePercent() instead of hooks when FreeRTOS is
-//  updated to version 10.5.1 or later.
+#define URT configRUN_TIME_COUNTER_TYPE
 
-#if defined(CONFIG_ESP32_DEFAULT_CPU_FREQ_240) || defined(CONFIG_ESP32S3_DEFAULT_CPU_FREQ_240)
-#define NAOS_CPU_MAX_IDLE_CALLS 368000.f
-#elif defined(CONFIG_ESP32_DEFAULT_CPU_FREQ_160) || defined(CONFIG_ESP32S3_DEFAULT_CPU_FREQ_160)
-#define NAOS_CPU_MAX_IDLE_CALLS 245333.f
-#elif defined(CONFIG_ESP32_DEFAULT_CPU_FREQ_80) || defined(CONFIG_ESP32S3_DEFAULT_CPU_FREQ_80)
-#define NAOS_CPU_MAX_IDLE_CALLS 122666.f
-#else
-#error "Unsupported CPU Frequency"
-#endif
-
-static uint64_t naos_cpu_idle0 = 0;
-static uint64_t naos_cpu_idle1 = 0;
+static TaskHandle_t naos_cpu_handles[2] = {0};
+static URT naos_cpu_system_run_time = 0;
+static URT naos_cpu_task_run_time[2] = {0};
 static float naos_cpu_usage[2] = {0};
 
-static bool naos_cpu_hook0() {
-  naos_cpu_idle0++;
-  return false;
-}
-
-static bool naos_cpu_hook1() {
-  naos_cpu_idle1++;
-  return false;
-}
-
 static void naos_cpu_update() {
-  // get counters
-  float idle0 = (float)naos_cpu_idle0;
-  float idle1 = (float)naos_cpu_idle1;
+  // get total system run time
+  URT total_system_rt = portGET_RUN_TIME_COUNTER_VALUE();
 
-  // reset counters
-  naos_cpu_idle0 = 0;
-  naos_cpu_idle1 = 0;
-
-  // clamp counters
-  if (idle0 > NAOS_CPU_MAX_IDLE_CALLS) {
-    idle0 = NAOS_CPU_MAX_IDLE_CALLS;
-  }
-  if (idle1 > NAOS_CPU_MAX_IDLE_CALLS) {
-    idle1 = NAOS_CPU_MAX_IDLE_CALLS;
+  // get total idle run times
+  URT total_idle_rt[2] = {0};
+  for (size_t i = 0; i < 2; i++) {
+    TaskStatus_t status;
+    vTaskGetInfo(naos_cpu_handles[i], &status, pdFALSE, eRunning);
+    total_idle_rt[i] = status.ulRunTimeCounter;
   }
 
-  // calculate new usage
-  float usage0 = 1.f - idle0 / NAOS_CPU_MAX_IDLE_CALLS;
-  float usage1 = 1.f - idle1 / NAOS_CPU_MAX_IDLE_CALLS;
+  // calculate differences
+  URT system_rtd = total_system_rt - naos_cpu_system_run_time;
+  URT idle_rtd[2] = {0};
+  for (size_t i = 0; i < 2; i++) {
+    idle_rtd[i] = total_idle_rt[i] - naos_cpu_task_run_time[i];
+  }
 
-  // add to usage
-  naos_cpu_usage[0] = (naos_cpu_usage[0] * 4 + usage0) / 5;
-  naos_cpu_usage[1] = (naos_cpu_usage[1] * 4 + usage1) / 5;
+  // calculate CPU usages
+  for (size_t i = 0; i < 2; i++) {
+    naos_cpu_usage[i] = 1 - (float)idle_rtd[i] / (float)system_rtd;
+  }
+
+  // update values
+  naos_cpu_system_run_time = total_system_rt;
+  for (size_t i = 0; i < 2; i++) {
+    naos_cpu_task_run_time[i] = total_idle_rt[i];
+  }
 }
 
 static naos_metric_t naos_cpu_metrics[] = {{
@@ -73,12 +59,16 @@ void naos_cpu_init() {
     naos_metrics_add(metric);
   }
 
-  // register hooks
-  ESP_ERROR_CHECK(esp_register_freertos_idle_hook_for_cpu(naos_cpu_hook0, 0));
-  ESP_ERROR_CHECK(esp_register_freertos_idle_hook_for_cpu(naos_cpu_hook1, 1));
+  // get task handles
+  naos_cpu_handles[0] = xTaskGetHandle("IDLE0");
+  naos_cpu_handles[1] = xTaskGetHandle("IDLE1");
+  if (naos_cpu_handles[0] == NULL || naos_cpu_handles[1] == NULL) {
+    ESP_ERROR_CHECK(ESP_FAIL);
+    return;
+  }
 
   // start update timer
-  naos_repeat("naos-cpu", 1000, naos_cpu_update);
+  naos_repeat("naos-cpu", 250, naos_cpu_update);
 }
 
 void naos_cpu_get(float *cpu0, float *cpu1) {
