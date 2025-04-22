559f75e00ef2932866be2249da1d97abf160f05c
Joël Gähwiler <joel.gaehwiler@gmail.com>
2025-04-22 21:50:59 +0200

    pkg: revert back to fetching all changes

--- pkg/utils/git.go
+++ pkg/utils/git.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"io"
 	"os/exec"
-	"strings"
 )
 
 // Clone will check out the provided repository set it to the specified version
@@ -30,14 +29,8 @@ func Clone(repo, path, commit string, ignoredSubmodules []string, out io.Writer)
 // Fetch will update the remote repository and update all submodules
 // accordingly.
 func Fetch(path, commit string, ignoredSubmodules []string, out io.Writer) error {
-	// unprefix commit
-	_commit := commit
-	if strings.HasPrefix(commit, "origin/") {
-		_commit = commit[7:]
-	}
-
 	// fetch repo
-	cmd := exec.Command("git", "fetch", "origin", _commit)
+	cmd := exec.Command("git", "fetch", "origin")
 	cmd.Stdout = out
 	cmd.Stderr = out
 	cmd.Dir = path
@@ -48,8 +41,8 @@ func Fetch(path, commit string, ignoredSubmodules []string, out io.Writer) error
 
 	// reset repo to specific version
 	cmd = exec.Command("git", "reset", "--hard", commit)
-	cmd.Stderr = out
 	cmd.Stdout = out
+	cmd.Stderr = out
 	cmd.Dir = path
 	err = cmd.Run()
 	if err != nil {
