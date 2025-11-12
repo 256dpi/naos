e81cb9ad333f431c3a6109e5a49a37f8f0e3dc80
Joël Gähwiler <joel.gaehwiler@gmail.com>
2025-11-12 20:37:27 +0100

    swift: added mobile target to test iOS build compatibility

---
+++ swift/NAOS-Mobile/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  NAOS-Mobile
+//
+//  Created by Joël Gähwiler on 12.11.2025.
+//  Copyright © 2025 Joël Gähwiler. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift/NAOS-Mobile/MobileApp.swift
@@ -0,0 +1,18 @@
+//
+//  NAOS_MobileApp.swift
+//  NAOS-Mobile
+//
+//  Created by Joël Gähwiler on 12.11.2025.
+//  Copyright © 2025 Joël Gähwiler. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
