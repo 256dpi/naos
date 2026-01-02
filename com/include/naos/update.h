91cf301c6f655024efaffc8614b505ad10fcade9
Joël Gähwiler <joel.gaehwiler@gmail.com>
2026-01-02 17:37:28 +0100

    com/update: make sync again (no callback)

--- com/include/naos/update.h
+++ com/include/naos/update.h
@@ -1,50 +1,39 @@
 #ifndef NAOS_UPDATE_H
 #define NAOS_UPDATE_H
 
+#include <stdbool.h>
 #include <stdint.h>
 #include <stddef.h>
 
 /**
- * The update status events.
- */
-typedef enum {
-  NAOS_UPDATE_READY,
-  NAOS_UPDATE_DONE,
-} naos_update_event_t;
-
-/**
- * The update callback.
- */
-typedef void (*naos_update_callback_t)(naos_update_event_t event);
-
-/**
- * Begin a new update. A previously started update will be aborted.
- * The callback will be called when the update is ready/done. If absent,
- * the update will be started synchronously and be ready when this function
- * returns.
+ * Begin a new update. Any previously started update will be aborted.
  *
  * @param size The size of the update.
- * @param cb The callback to call when the update is ready/done.
+ * @return True if the update was started successfully, false otherwise.
  */
-void naos_update_begin(size_t size, naos_update_callback_t cb);
+bool naos_update_begin(size_t size);
 
 /**
  * Write a chunk of data to the update.
  *
  * @param chunk The chunk of data.
  * @param len The length of the chunk.
+ * @return True if the chunk was written successfully, false otherwise.
  */
-void naos_update_write(const uint8_t *chunk, size_t len);
+bool naos_update_write(const uint8_t *chunk, size_t len);
 
 /**
  * Abort the update.
+ *
+ * @return True if the update was aborted successfully, false otherwise.
  */
-void naos_update_abort();
+bool naos_update_abort();
 
 /**
- * Finish the update. If no callback has been set, this function will block
- * until the update is done.
+ * Finish the update.
+ *
+ * @return True if the update was finished successfully, false otherwise.
  */
-void naos_update_finish();
+bool naos_update_finish();
 
 #endif  // NAOS_UPDATE_H
--- com/src/update.c
+++ com/src/update.c
@@ -7,8 +7,10 @@
 #include <esp_system.h>
 #include <string.h>
 
-#include "update.h"
 #include "utils.h"
+
+// TODO: Remove events, use built-in replies.
+// TODO: Verify update size and sequential writes.
 
 #define NAOS_UPDATE_ENDPOINT 0x2
 
@@ -19,88 +21,17 @@ typedef enum {
   NAOS_UPDATE_FINISH,
 } naos_update_cmd_t;
 
+typedef enum {
+  NAOS_UPDATE_READY,
+  NAOS_UPDATE_DONE,
+} naos_update_event_t;
+
 static naos_mutex_t naos_update_mutex;
-static naos_update_callback_t naos_update_callback = NULL;
 static const esp_partition_t *naos_update_partition = NULL;
 static size_t naos_update_size = 0;
 static esp_ota_handle_t naos_update_handle = 0;
 static uint16_t naos_update_session = 0;
 static bool naos_update_block = false;
-
-static void naos_update_begin_task() {
-  // acquire mutex
-  naos_lock(naos_update_mutex);
-
-  // log message
-  ESP_LOGI(NAOS_LOG_TAG, "naos_update_begin_task: preparing update...");
-
-  // begin update (without full flash erase)
-  ESP_ERROR_CHECK(esp_ota_begin(naos_update_partition, OTA_WITH_SEQUENTIAL_WRITES, &naos_update_handle));
-
-  // release mutex
-  naos_unlock(naos_update_mutex);
-
-  // call callback if available
-  if (naos_update_callback != NULL) {
-    naos_update_callback(NAOS_UPDATE_READY);
-  }
-
-  // send reply to session
-  if (naos_update_session != 0) {
-    uint8_t event = NAOS_UPDATE_READY;
-    naos_msg_send((naos_msg_t){
-        .session = naos_update_session,
-        .endpoint = NAOS_UPDATE_ENDPOINT,
-        .data = &event,
-        .len = 1,
-    });
-  }
-}
-
-static void naos_update_finish_task() {
-  // acquire mutex
-  naos_lock(naos_update_mutex);
-
-  // end update
-  ESP_ERROR_CHECK(esp_ota_end(naos_update_handle));
-
-  // set boot partition
-  ESP_ERROR_CHECK(esp_ota_set_boot_partition(naos_update_partition));
-
-  // log message
-  ESP_LOGI(NAOS_LOG_TAG, "naos_update_finish_task: update finished");
-
-  // set block
-  naos_update_block = true;
-
-  // release mutex
-  naos_unlock(naos_update_mutex);
-
-  // call callback if available
-  if (naos_update_callback != NULL) {
-    naos_update_callback(NAOS_UPDATE_DONE);
-  }
-
-  // send reply to session
-  if (naos_update_session != 0) {
-    uint8_t event = NAOS_UPDATE_DONE;
-    naos_msg_send((naos_msg_t){
-        .session = naos_update_session,
-        .endpoint = NAOS_UPDATE_ENDPOINT,
-        .data = &event,
-        .len = 1,
-    });
-  }
-
-  // log message
-  ESP_LOGI(NAOS_LOG_TAG, "naos_update_finish_task: rebooting in one second...");
-
-  // wait one second
-  naos_delay(1000);
-
-  // restart system
-  esp_restart();
-}
 
 static naos_msg_reply_t naos_update_process(naos_msg_t msg) {
   // check length
@@ -122,7 +53,7 @@ static naos_msg_reply_t naos_update_process(naos_msg_t msg) {
 
   // handle command
   switch (cmd) {
-    case NAOS_UPDATE_BEGIN:
+    case NAOS_UPDATE_BEGIN: {
       // command structure:
       // SIZE (4)
 
@@ -135,15 +66,25 @@ static naos_msg_reply_t naos_update_process(naos_msg_t msg) {
       uint32_t size = 0;
       memcpy(&size, msg.data, 4);
 
+      // begin update
+      naos_update_begin(size);
+
       // set session
       naos_update_session = msg.session;
 
-      // begin update
-      naos_update_begin(size, NULL);
+      // send reply to session
+      uint8_t event = NAOS_UPDATE_READY;
+      naos_msg_send((naos_msg_t){
+          .session = msg.session,
+          .endpoint = NAOS_UPDATE_ENDPOINT,
+          .data = &event,
+          .len = 1,
+      });
 
       return NAOS_MSG_OK;
+    }
 
-    case NAOS_UPDATE_WRITE:
+    case NAOS_UPDATE_WRITE: {
       // command structure:
       // ACKED (1) | DATA (*)
 
@@ -164,8 +105,9 @@ static naos_msg_reply_t naos_update_process(naos_msg_t msg) {
       naos_update_write(msg.data + 1, msg.len - 1);
 
       return acked ? NAOS_MSG_ACK : NAOS_MSG_OK;
+    }
 
-    case NAOS_UPDATE_ABORT:
+    case NAOS_UPDATE_ABORT: {
       // check length
       if (msg.len != 0) {
         return NAOS_MSG_INVALID;
@@ -179,9 +121,13 @@ static naos_msg_reply_t naos_update_process(naos_msg_t msg) {
       // abort update
       naos_update_abort();
 
-      return NAOS_MSG_ACK;
+      // clear session
+      naos_update_session = 0;
 
-    case NAOS_UPDATE_FINISH:
+      return NAOS_MSG_ACK;
+    }
+
+    case NAOS_UPDATE_FINISH: {
       // check length
       if (msg.len != 0) {
         return NAOS_MSG_INVALID;
@@ -195,18 +141,34 @@ static naos_msg_reply_t naos_update_process(naos_msg_t msg) {
       // finish update
       naos_update_finish();
 
+      // clear session
+      naos_update_session = 0;
+
+      // send reply to session
+      uint8_t event = NAOS_UPDATE_DONE;
+      naos_msg_send((naos_msg_t){
+          .session = msg.session,
+          .endpoint = NAOS_UPDATE_ENDPOINT,
+          .data = &event,
+          .len = 1,
+      });
+
       return NAOS_MSG_OK;
+    }
 
     default:
       return NAOS_MSG_UNKNOWN;
   }
 }
 
-static void naos_update_cleanup(uint16_t ref) {
-  // check sessions
-  if (naos_update_session == ref) {
+static void naos_update_cleanup(uint16_t session) {
+  // check if active session is lost
+  if (naos_update_session == session) {
     // abort update
     naos_update_abort();
+
+    // clear session
+    naos_update_session = 0;
   }
 }
 
@@ -223,7 +185,7 @@ void naos_update_init() {
   });
 }
 
-void naos_update_begin(size_t size, naos_update_callback_t cb) {
+bool naos_update_begin(size_t size) {
   // acquire mutex
   naos_lock(naos_update_mutex);
 
@@ -231,44 +193,43 @@ void naos_update_begin(size_t size, naos_update_callback_t cb) {
   if (naos_update_block) {
     ESP_LOGE(NAOS_LOG_TAG, "naos_update_begin: blocked");
     naos_unlock(naos_update_mutex);
-    return;
+    return false;
   }
 
   // abort a previous update and discard its result
   if (naos_update_handle != 0) {
+    ESP_LOGE(NAOS_LOG_TAG, "naos_update_begin: aborting previous update...");
     ESP_ERROR_CHECK_WITHOUT_ABORT(esp_ota_abort(naos_update_handle));
     naos_update_handle = 0;
   }
-
-  // log message
-  ESP_LOGI(NAOS_LOG_TAG, "naos_update_begin: starting update...");
-
-  // store callback
-  naos_update_callback = cb;
 
   // get update partition
   naos_update_partition = esp_ota_get_next_update_partition(NULL);
   if (naos_update_partition == NULL) {
     ESP_LOGE(NAOS_LOG_TAG, "naos_update_begin: no partition available");
     naos_unlock(naos_update_mutex);
-    return;
+    return false;
   }
+
+  // log message
+  ESP_LOGI(NAOS_LOG_TAG, "naos_update_begin: beginning update...");
 
   // store size
   naos_update_size = size;
 
+  // begin update (without full flash erase)
+  ESP_ERROR_CHECK(esp_ota_begin(naos_update_partition, OTA_WITH_SEQUENTIAL_WRITES, &naos_update_handle));
+
+  // log message
+  ESP_LOGI(NAOS_LOG_TAG, "naos_update_begin: update begun!");
+
   // release mutex
   naos_unlock(naos_update_mutex);
 
-  // begin update async or sync
-  if (naos_update_callback != NULL) {
-    naos_run("naos-update-b", 4096, 1, naos_update_begin_task);
-  } else {
-    naos_update_begin_task();
-  }
+  return true;
 }
 
-void naos_update_write(const uint8_t *chunk, size_t len) {
+bool naos_update_write(const uint8_t *chunk, size_t len) {
   // acquire mutex
   naos_lock(naos_update_mutex);
 
@@ -276,24 +237,26 @@ void naos_update_write(const uint8_t *chunk, size_t len) {
   if (naos_update_block) {
     ESP_LOGE(NAOS_LOG_TAG, "naos_update_write: blocked");
     naos_unlock(naos_update_mutex);
-    return;
+    return false;
   }
 
   // check handle
   if (naos_update_handle == 0) {
     ESP_LOGE(NAOS_LOG_TAG, "naos_update_write: missing handle");
     naos_unlock(naos_update_mutex);
-    return;
+    return false;
   }
 
   // write chunk
-  ESP_ERROR_CHECK(esp_ota_write(naos_update_handle, (const void *)chunk, len));
+  ESP_ERROR_CHECK(esp_ota_write(naos_update_handle, chunk, len));
 
   // release mutex
   naos_unlock(naos_update_mutex);
+
+  return true;
 }
 
-void naos_update_abort() {
+bool naos_update_abort() {
   // acquire mutex
   naos_lock(naos_update_mutex);
 
@@ -301,7 +264,7 @@ void naos_update_abort() {
   if (naos_update_block) {
     ESP_LOGE(NAOS_LOG_TAG, "naos_update_abort: blocked");
     naos_unlock(naos_update_mutex);
-    return;
+    return false;
   }
 
   // abort a previous update and discard its result
@@ -311,17 +274,17 @@ void naos_update_abort() {
   }
 
   // clear state
-  naos_update_callback = NULL;
   naos_update_partition = NULL;
   naos_update_size = 0;
   naos_update_handle = 0;
-  naos_update_session = 0;
 
   // release mutex
   naos_unlock(naos_update_mutex);
+
+  return true;
 }
 
-void naos_update_finish() {
+bool naos_update_finish() {
   // acquire mutex
   naos_lock(naos_update_mutex);
 
@@ -329,26 +292,39 @@ void naos_update_finish() {
   if (naos_update_block) {
     ESP_LOGE(NAOS_LOG_TAG, "naos_update_finish: blocked");
     naos_unlock(naos_update_mutex);
-    return;
+    return false;
   }
 
   // check handle
   if (naos_update_handle == 0) {
     ESP_LOGE(NAOS_LOG_TAG, "naos_update_finish: missing handle");
     naos_unlock(naos_update_mutex);
-    return;
+    return false;
   }
 
   // log message
   ESP_LOGI(NAOS_LOG_TAG, "naos_update_finish: finishing update...");
 
+  // end update
+  ESP_ERROR_CHECK(esp_ota_end(naos_update_handle));
+
+  // set boot partition
+  ESP_ERROR_CHECK(esp_ota_set_boot_partition(naos_update_partition));
+
+  // log message
+  ESP_LOGI(NAOS_LOG_TAG, "naos_update_finish: update finished!");
+
+  // set block
+  naos_update_block = true;
+
   // release mutex
   naos_unlock(naos_update_mutex);
 
-  // finish update async or sync
-  if (naos_update_callback != NULL) {
-    naos_run("naos-update-f", 4096, 1, naos_update_finish_task);
-  } else {
-    naos_update_finish_task();
-  }
+  // log message
+  ESP_LOGI(NAOS_LOG_TAG, "naos_update_finish: rebooting in 1s...");
+
+  // restart in one second
+  naos_defer("naos-restart", 1000, esp_restart);
+
+  return true;
 }
