d0ae6deb1505212491a450363526c8c81a3513a5
Joël Gähwiler <joel.gaehwiler@gmail.com>
2026-03-29 14:01:26 +0200

    ci: use single sync script

--- .github/workflows/sync.sh
+++ .github/workflows/sync.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-FOLDER="web"
+FOLDER=$1
+REPO_NAME=$2
 GITHUB_USERNAME="256dpi"
-REPO_NAME="NAOS.js"
 BRANCH_NAME="main"
 BASE=$(pwd)
 
 git config --global user.name "GitHub Actions"
 git config --global user.email "actions@example.com"
 
-echo "Cloning folder $FOLDER and pushing to $GITHUB_USERNAME"
+echo "Syncing folder $FOLDER to $GITHUB_USERNAME/$REPO_NAME"
 
 # Clone the target repository
 git clone --depth 1 https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$REPO_NAME.git &> /dev/null
@@ -21,7 +21,7 @@ cd $REPO_NAME
 find . -maxdepth 1 ! -name '.git' -exec rm -rf {} \;
 
 # Copy the contents of the source directory to the repository root
-shopt -s dotglob # also copy hidden files
+shopt -s dotglob
 cp -r $BASE/$FOLDER/* .
 
 # Commit and push changes if there are any
