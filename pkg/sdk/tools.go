7f7fe7297fa896d09befb600ba05293e1bb6773c
Joël Gähwiler <joel.gaehwiler@gmail.com>
2026-08-09 12:39:51 +0200

    pkg: install ninja with toolchain

--- pkg/sdk/tools.go
+++ pkg/sdk/tools.go
@@ -27,39 +27,60 @@ func InstallToolchain(version string, out io.Writer) (string, error) {
 	toolsDir := filepath.Join(base, toolsKey)
 	flagFile := filepath.Join(toolsDir, ".installed")
 
-	// check flag
-	ok, err := utils.Exists(flagFile)
+	// prepare commands
+	var commands [][]string
+
+	// install toolchain if not yet installed
+	installed, err := utils.Exists(flagFile)
 	if err != nil {
 		return "", err
-	} else if ok {
+	} else if !installed {
+		commands = append(commands, []string{filepath.Join(idfDir, "install.sh"), "all"})
+	}
+
+	// install ninja if missing ("ninja" is an on request tool and thus not
+	// covered by the install script, but yields notably faster builds than
+	// the "Unix Makefiles" fallback)
+	ninja, err := utils.Exists(filepath.Join(toolsDir, "tools", "ninja"))
+	if err != nil {
+		return "", err
+	} else if !ninja {
+		commands = append(commands, []string{filepath.Join(idfDir, "tools", "idf_tools.py"), "install", "ninja"})
+	}
+
+	// return early if there is nothing to do
+	if len(commands) == 0 {
 		return toolsDir, nil
 	}
 
-	// prepare command
-	cmd := exec.Command(filepath.Join(idfDir, "install.sh"), "all")
+	// run commands
+	for _, command := range commands {
+		// prepare command
+		cmd := exec.Command(command[0], command[1:]...)
 
-	// connect output and inputs
-	cmd.Stdout = out
-	cmd.Stderr = out
+		// connect output and inputs
+		cmd.Stdout = out
+		cmd.Stderr = out
 
-	// inherit current environment
-	cmd.Env = os.Environ()
+		// inherit current environment
+		cmd.Env = os.Environ()
 
-	// go through all env variables
-	for i, str := range cmd.Env {
-		if strings.HasPrefix(str, "PWD=") {
-			// override shell working directory
-			cmd.Env[i] = "PWD=" + toolsDir
+		// go through all env variables
+		for i, str := range cmd.Env {
+			if strings.HasPrefix(str, "PWD=") {
+				// override shell working directory
+				cmd.Env[i] = "PWD=" + toolsDir
+			}
 		}
-	}
 
-	// set IDF tools path
-	cmd.Env = append(cmd.Env, "IDF_TOOLS_PATH="+toolsDir)
+		// set IDF tools path
+		cmd.Env = append(cmd.Env, "IDF_TOOLS_PATH="+toolsDir)
 
-	// run command
-	err = cmd.Run()
-	if err != nil {
-		return "", err
+		// run command
+		err = cmd.Run()
+		if err != nil {
+			return "", err
+		}
 	}
 
 	// write flag
