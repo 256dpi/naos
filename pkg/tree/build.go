bd4e7add177cee85ea8b6d37db3a6c02fa6c76bc
Joël Gähwiler <joel.gaehwiler@gmail.com>
2026-08-09 13:01:31 +0200

    pkg: drop stale todo

--- pkg/tree/build.go
+++ pkg/tree/build.go
@@ -11,7 +11,6 @@ import (
 	"github.com/256dpi/naos/pkg/utils"
 )
 
-// TODO: Changing embeds in a v4 projects requires a clean.
 // TODO: Changing target needs a reconfigure and clean.
 
 // Partitions defines a percentage based partitioning scheme.
